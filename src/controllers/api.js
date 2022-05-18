df2188397ded884ad68f63022f87d4b9d0afdaaa
Dan Hadar <danhad@menoramivt.co.il>
2022-05-18 23:00:25 +0300

    fixed syntax

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,10 +1,10 @@
 const router = require( "express" ).Router();
-const SiteVisitsManager = require( '../services/SiteVisitsManager' );
+const { calculateSessions, numSessions, medianSessionsLength, numUniqueVisitedSites } = require( '../services/SiteVisitsManager' );
 
 router.get( '/calcSessions', async ( req, res ) => {
     try {
         console.log( `api.js/calcSessions: new Req for sessions calculation` );
-        await SiteVisitsManager.calculateSessions();
+        await calculateSessions();
         res.status( 200 ).send( 'Success' );
     } catch ( err ) {
         console.error( `Failed to load files and calculate sessions, ${ err.stack }` );
@@ -16,7 +16,7 @@ router.get( '/numSessions/:siteUrl', async ( req, res ) => {
     const { siteUrl } = req.params;
     try {
         console.log( `api.js/numSessions: new Req for site ${ siteUrl }` );
-        const num = await SiteVisitsManager.numSessions( siteUrl );
+        const num = await numSessions( siteUrl );
         res.status( 200 ).send( `Num sessions for site ${ siteUrl } = ${ num } ` );
         console.log( `api.js/numSessions: finished Req for site ${ siteUrl } successfully` );
     } catch ( err ) {
@@ -29,8 +29,8 @@ router.get( '/medianSessionsLength/:siteUrl', async ( req, res ) => {
     const { siteUrl } = req.params;
     try {
         console.log( `api.js/medianSessionsLength: new Req for site ${ siteUrl }` );
-        const num = await SiteVisitsManager.medianSessionsLength( siteUrl );
-        res.status( 200 ).send( `Median session length for site ${ siteUrl } = ${ num } ` );
+        const medSessionLength = await medianSessionsLength( siteUrl );
+        res.status( 200 ).send( `Median session length for site ${ siteUrl } = ${ medSessionLength } ` );
         console.log( `api.js/medianSessionsLength: finished Req for site ${ siteUrl } successfully` );
     } catch ( err ) {
         console.error( `Failed to get site median of session length for site ${ siteUrl }, ${ err.stack }` );
@@ -42,8 +42,8 @@ router.get( '/numUniqueVisitedSites/:visitorId', async ( req, res ) => {
     const { visitorId } = req.params;
     try {
         console.log( `api.js/numUniqueVisitedSites: new Req for visitorId ${ visitorId }` );
-        const num = await SiteVisitsManager.numUniqueVisitedSites( visitorId );
-        res.status( 200 ).send( `Num of unique sites for ${ visitorId } = ${ num } ` );
+        const uniqueVisitedSitesNum = await numUniqueVisitedSites( visitorId );
+        res.status( 200 ).send( `Num of unique sites for ${ visitorId } = ${ uniqueVisitedSitesNum } ` );
         console.log( `api.js/numUniqueVisitedSites: finished Req for visitorId ${ visitorId } successfully` );
     } catch ( err ) {
         console.error( `Failed to get num of unique sites for visitor ${ visitorId }, ${ err.stack }` );
